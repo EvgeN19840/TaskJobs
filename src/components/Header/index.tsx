d08cd408b9c8e18ef903849456871f9a3864dc1f
EvgeN19840 <107992155+EvgeN19840@users.noreply.github.com>
2023-05-24 16:10:22 +0400

    burger

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,49 +1,66 @@
 import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, NavLink } from "react-router-dom";
+import avatar from "../picturs/avatar.jpg"
 
-import {NavLink} from "react-router-dom"
 import "./styles.css";
 
 const Header = () => {
   const [isActive, setIsActive] = useState(false);
+  const [isInfoVisible, setIsInfoVisible] = useState(false);
 
   const toggleActive = () => {
     setIsActive(!isActive);
+    setIsInfoVisible(!isInfoVisible);
   };
+
   useEffect(() => {
     document.body.classList.toggle("lock", isActive);
   }, [isActive]);
-  return (
-    <header className="main-header">
-      <div className="menu-header">
-        {/* <nav className={`navbar-link body ${isActive ? "active" : ""}`}>
-          <ul className="navbar-link">
-          <NavLink
-              className="navbar-link-button"
-              // to={routeMainPage()}
-              activeClassName={"linkActive"}
-              onClick={toggleActive}
-            >
-              Список постов
-            </NavLink>
 
-            <NavLink
-              className="navbar-link-button"
-              // to={routeSearchPage()}
-              activeClassName={"linkActive"}
-              onClick={toggleActive}
-            >
-              Обо мне
-            </NavLink>
-          </ul>
-        </nav> */}
-        <div
-          className={`header-burger body ${isActive ? "active" : ""}`}
-          onClick={toggleActive}
-        >
-          <span></span>
+  return (
+    <Router>
+      <header className="main-header">
+        <div className="menu-header">
+       
+          <nav className={`navbar-link body ${isActive ? "active" : ""}`}>
+          {isInfoVisible && (
+            <div className="user-info">
+              <img src={avatar} alt="Avatar" />
+              <div>
+                <p>Евгений</p>
+                <p>example@mail.com</p>
+              </div>
+            </div>
+          )}
+            <ul className="navbar-link">
+              <NavLink
+                className="navbar-link-button"
+                to="/posts"
+                activeClassName={"linkActive"}
+                onClick={toggleActive}
+              >
+                Список постов
+              </NavLink>
+
+              <NavLink
+                className="navbar-link-button"
+                to="/about"
+                activeClassName={"linkActive"}
+                onClick={toggleActive}
+              >
+                Обо мне
+              </NavLink>
+            </ul>
+          </nav>
+          <div
+            className={`header-burger body ${isActive ? "active" : ""}`}
+            onClick={toggleActive}
+          >
+            <span></span>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </Router>
   );
 };
 
